621bcb709d101b910c42b4e00e648453256cf1e4
inietov <inietov@gmail.com>
2015-08-01 14:19:50 -0500

    Commit de reemplazo

--- Nucleus.py
+++ Nucleus.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 
 import shodan
+import GeoIP
 import sys
 
 # Configurar API Key
-API_KEY = "boGy4JLA2KaTNEIZIn61qaV7Mwf3D8AC"
+API_KEY = "API_Shodan"
 
 if len(sys.argv) == 1:
 	print 'Uso: %s <Query>' % sys.argv[0] 
@@ -13,11 +14,17 @@ try:
 	api = shodan.Shodan(API_KEY)
 	# Lanza la busqueda
 	query = ' '.join(sys.argv[1:])
-	res = api.search(query)
+	res = api.search(query, limit=200, offset=None)
+	lista_paises = []
 	# Recorre resultados para imprimir ip's que coinciden con la busqueda
 	for serv in res['matches']: 
-		print  "Pais %s" % res.get('country_name')
+		gi = GeoIP.new(GeoIP.GEOIP_MEMORY_CACHE)
+		#print  serv['ip_str'] + '-' + gi.country_name_by_addr(serv['ip_str'])
+		lista_paises.append(gi.country_name_by_addr(serv['ip_str']))
 
+	lista_paises.sort()
+	print lista_paises
+	print "Resultados en lista: " + str(len(lista_paises))
 	print "Resultados encontrados: %d" % res['total']
 	print "Resultados devueltos: %d" % len(res['matches'])
 
